a3c04b3426169ef5bcc3b3344f83c949e5dcd47a
lobodev-joni <37211435+lobodev-joni@users.noreply.github.com>
2018-03-12 17:26:57 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,4 +7,4 @@
 // M::Hi from master branch Aptana on online remote repo
 // M::You there?
 
-// M::Hi Again from MASTER. This has to go live now.
+// M::Hi Again from MASTER. This has to go live now. ...
